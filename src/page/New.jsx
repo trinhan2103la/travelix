c417fffcdc05376da048f3520e48f22508d5000e
trinhan2103la <trinhan212003la@gmail.com>
2024-11-04 14:04:35 +0700

    [Update img in New page]

--- src/page/New.jsx
+++ src/page/New.jsx
@@ -106,43 +106,63 @@ const New = () => {
               <div className="row margin-top">
                 <div className="one-8 num-8 c-12">
                   <div className="blog-container-post">
-                    <div className="blog-container-post--time">
-                      <span>01</span>
-                      <span>July, 2017</span>
-                    </div>
-                    <img
-                      src="https://tranhoangkhang1212.github.io/travelix/assets/images/blog_1.jpg"
-                      alt=""
-                    />
+                    <ul>
+                      <li>
+                        <a href="#">by Lore Papp |</a>
+                      </li>
+                      <li>
+                        <a href="#"> Uncategorized |</a>
+                      </li>
+                      <li>
+                        <a href="#"> 3 Comments</a>
+                      </li>
+                    </ul>
+                    <a href="#" className="blog-container-post--title">
+                      Try these new dream destinations
+                    </a>
+                    <p>
+                      Aenean in lacus ligula. Phasellus euismod gravida eros.
+                      Aenean nec ipsum aliquet, pharetra magna id, interdum
+                      sapien. Etiam id lorem eu nisl pellentesque semper. Nullam
+                      tincidunt metus placerat, suscipit leo ut, tempus nulla.
+                      Fusce at eleifend tellus. Ut eleifend dui nunc, non
+                      fermentum qua.
+                    </p>
+                    <a href="#" className="blog-container-post--btn">
+                      Read more
+                    </a>
                   </div>
                 </div>
                 <div className="one-4 num-4 c-12">
                   <div className="blog-container-sidebar">
                     <div className="container-sidebar--list">
-                      <span>Categories</span>
-                      <ul>
-                        <li>
-                          <a href="#">Travel</a>
-                        </li>
-                        <li>
-                          <a href="#">Exotic Destinations</a>
-                        </li>
-                        <li>
-                          <a href="#">City Breaks</a>
-                        </li>
-                        <li>
-                          <a href="#">Travel Tips</a>
-                        </li>
-                        <li>
-                          <a href="#">Lifestyle & Travel</a>
-                        </li>
-                        <li>
-                          <a href="#">Noncat</a>
-                        </li>
-                        <li>
-                          <a href="#">Flower</a>
-                        </li>
-                      </ul>
+                      <span>Instagram</span>
+                      <div className="container-sidebar--list-img">
+                        <img
+                          src="https://tranhoangkhang1212.github.io/travelix/assets/images/xoffer_4.jpg.pagespeed.ic.sP-uVX8-UR.webp"
+                          alt=""
+                        />
+                        <img
+                          src="https://tranhoangkhang1212.github.io/travelix/assets/images/xoffer_1.jpg.pagespeed.ic.LnLHOsFCJq.webp"
+                          alt=""
+                        />
+                        <img
+                          src="https://tranhoangkhang1212.github.io/travelix/assets/images/gallery_3.jpg"
+                          alt=""
+                        />
+                        <img
+                          src="https://tranhoangkhang1212.github.io/travelix/assets/images/gallery_4.jpg"
+                          alt=""
+                        />
+                        <img
+                          src="https://tranhoangkhang1212.github.io/travelix/assets/images/gallery_5.jpg"
+                          alt=""
+                        />
+                        <img
+                          src="https://tranhoangkhang1212.github.io/travelix/assets/images/gallery_6.jpg"
+                          alt=""
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
